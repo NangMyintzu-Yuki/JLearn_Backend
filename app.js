aa560a1adb7328f81e280ef0d5932191ec3f5c23
Nang Myintzu <nangmyintzu89@gmail.com>
2025-08-16 22:11:21 +0630

    test

--- app.js
+++ app.js
@@ -1,63 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const { cors } = require('./middleware/cors.middleware.js')
+require('dotenv').config();
+const express = require('express');
+const { cors } = require('./middleware/cors.middleware.js');
+const expressFileUpload = require("express-fileupload");
 
 const app = express();
-let http = require('http').Server(app)
-const expressFileUpload = require("express-fileupload")
 
-
-// routers
-const authRoutes = require('./routes/auth.routes')
-const userRoutes = require('./routes/user.routes')
-const operatorRoutes = require('./routes/operator.routes')
-const consonantRoutes = require('./routes/consonant.routes')
-const kanjiRoutes = require('./routes/kanji.routes')
-const vocabRoutes = require("./routes/vocab.routes")
-const grammarRoutes = require("./routes/grammar.routes");
-const quizRoutes = require('./routes/quiz.routes');
-const userQuizSubmitRoutes = require('./routes/user_quiz_submit.routes');
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-//allow cors
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors);
-
-// file upload
 app.use(expressFileUpload());
 
-// api routes
-app.get("/", (req, res) => {
-    res.json({ message: "Server is OK!" })
-})
+// Routes
+app.get("/", (req, res) => res.json({ message: "Server is OK!" }));
 
-app.use("/", authRoutes)
-app.use("/", userRoutes)
-app.use("/", operatorRoutes)
-app.use("/", consonantRoutes)
-app.use("/", kanjiRoutes)
-app.use("/", vocabRoutes)
-app.use("/", grammarRoutes)
-app.use("/", quizRoutes)
-app.use("/", userQuizSubmitRoutes)
+app.use("/", require('./routes/auth.routes'));
+app.use("/", require('./routes/user.routes'));
+app.use("/", require('./routes/operator.routes'));
+app.use("/", require('./routes/consonant.routes'));
+app.use("/", require('./routes/kanji.routes'));
+app.use("/", require('./routes/vocab.routes'));
+app.use("/", require('./routes/grammar.routes'));
+app.use("/", require('./routes/quiz.routes'));
+app.use("/", require('./routes/user_quiz_submit.routes'));
 
-const PORT = process.env.APP_PORT || 3000;
-
-const listen = async (err) => {
-    if (err) {
-        console.log("Error");
-    }
-    console.log(`Server is Running on PORT - ${PORT} `);
-}
-http.listen(PORT, listen)
-
-// const listen = async (err) => {
-//     if (err) {
-//         logger.error(`${err.message}`);
-//     }
-//     socket.start(io);
-//     logger.info(`Server is Running on PORT - ${PORT}`);
-// };
-
-// http.listen(PORT, listen);
+module.exports = app;   // <-- export instead of listen
